c0ccf9f93a8150b9f70560ca7d6e02476c34a5b2
Liliya <lilyeka@gmail.com>
2019-04-15 16:47:39 +0500

    Initial Commit

---
+++ ConstraintsAsObjects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConstraintsAsObjects
+//
+//  Created by Лилия Левина on 15/04/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
